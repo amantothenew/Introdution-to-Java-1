d86cf235025ff4d2eb813585660094161e06085e
amantothenew <aman.pal@tothenew.com>
2025-06-30 15:38:02 +0530

    Added: Q4 solution

---
+++ Q4.java
@@ -0,0 +1,30 @@
+// Q4- Create a two dimensional array and display sum of each row and column
+
+public class Main {
+    public static void main(String[] args) {
+        int[][] arr = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+        int rows = arr.length;
+        int cols = arr[0].length;
+        System.out.println("Sum of each row:");
+        for (int i = 0; i < rows; i++) {
+            int rowSum = 0;
+            for (int j = 0; j < cols; j++) {
+                rowSum += arr[i][j];
+            }
+            System.out.println(i + " " + rowSum);
+        }
+
+        System.out.println("\nSum of each column:");
+        for (int j = 0; j < cols; j++) {
+            int colSum = 0;
+            for (int i = 0; i < rows; i++) {
+                colSum += arr[i][j];
+            }
+            System.out.println(j + " " + colSum);
+        }
+    }
+}
